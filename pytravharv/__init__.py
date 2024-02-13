589794a7f824e6ef4215ed7f0572875b6e5136ab
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2024-02-13 11:17:17 +0100

    Add pytravharv package documentation

--- pytravharv/__init__.py
+++ pytravharv/__init__.py
@@ -1,1 +1,24 @@
-# init of the pyTravHarv package
+"""pytravharv
+
+.. module:: pytravharv
+:platform: Unix, Windows
+:synopsis: A package for traversing and harvesting RDF data by dereferencing URIs and asserting paths.
+
+.. moduleauthor:: Cedric Decruw <cedric.decruw@vliz.be>
+    
+"""
+
+from pytravharv.TargetStore import TargetStore
+from pytravharv.TravHarvConfigBuilder import (
+    TravHarvConfigBuilder,
+    TravHarvConfig,
+)
+from pytravharv.TravHarvExecuter import TravHarvExecutor
+
+
+__all__ = [
+    "TargetStore",
+    "TravHarvConfigBuilder",
+    "TravHarvConfig",
+    "TravHarvExecutor",
+]
