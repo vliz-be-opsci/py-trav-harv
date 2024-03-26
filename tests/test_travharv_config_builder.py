0cc6f40095af27cd6c7e4384f731b00ba18d251e
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2024-03-26 13:39:28 +0100

    Fix test_check_snooze assertion in test_travharv_config_builder.py

--- tests/test_travharv_config_builder.py
+++ tests/test_travharv_config_builder.py
@@ -172,7 +172,7 @@ def test_check_snooze(target_store_access):
     )
 
     test_pass = travharvconfigbuilder._check_snooze(10, "base_test.yml")
-    assert test_pass is True
+    assert test_pass is False
 
 
 @pytest.mark.usefixtures("target_store_access")
