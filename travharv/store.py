d99c86ee6360dc6ae4574e867ff279eb85fc7fd8
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2024-04-03 12:08:11 +0200

    Remove error handling for HTTP 400 response in select_subjects() method

--- travharv/store.py
+++ travharv/store.py
@@ -18,9 +18,6 @@ class TargetStoreAccess:
 
     def select_subjects(self, sparql) -> List[str]:
         result: Result = self._target.select(sparql)
-        # if result is (400, 'HTTP Error 400: ', None) then return empty list
-        if result == (400, "HTTP Error 400: ", None):
-            return []
 
         # todo convert response into list of subjects
         list_of_subjects = [row[0] for row in result]
