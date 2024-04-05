c9df2f1bb470dee5feae5c15d5d29edac1430676
Marc Portier <marc.portier@gmail.com>
2024-04-05 17:22:01 +0200

    comment for later

--- travharv/common.py
+++ travharv/common.py
@@ -17,7 +17,8 @@ QUERY_BUILDER = J2RDFSyntaxBuilder(
 )
 
 
-# TODO reconsider this common.py (as none of this seems/ feels common across travharv parts) --> issue #32
+# TODO reconsider this common.py
+# (as none of this seems/ feels common across travharv parts) --> issue #32
 
 
 def graph_name_to_uri(graph_name: str) -> str:
