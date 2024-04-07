4e6a062ff6689b6a975bb133a3b30aa9b2f1c2be
Marc Portier <marc.portier@gmail.com>
2024-04-07 18:26:25 +0200

    make test_path_assertion pass again

--- tests/test_path_assertion.py
+++ tests/test_path_assertion.py
@@ -4,63 +4,68 @@ from rdflib import URIRef
 from rdflib.query import ResultRow
 from util4tests import run_single_test
 
-import travharv.path_assertion as subj
+from travharv.path_assertion import SubjPropPathAssertion
 from travharv.config_build import AssertPath
 
 
-@pytest.mark.usefixtures("target_store_access")
-def test_init_subj_prop_path_assertion(target_store_access):
+@pytest.mark.usefixtures("decorated_rdf_stores")
+def test_init_subj_prop_path_assertion(decorated_rdf_stores):
     assertion_path = AssertPath("http://example.org/subject")
-    subj_prop_path_assertion = subj.SubjPropPathAssertion(
-        "http://example.org/subject",
-        assertion_path,
-        target_store_access,
-        {},
-        "urn:test",
-    )
-    assert subj_prop_path_assertion.subject == "http://example.org/subject"
-    assert subj_prop_path_assertion.assertion_path == assertion_path
+
+    for rdf_store in decorated_rdf_stores:
+        subj_prop_path_assertion = SubjPropPathAssertion(
+            "http://example.org/subject",
+            assertion_path,
+            rdf_store,
+            {},
+            "urn:test",
+        )
+        assert subj_prop_path_assertion.subject == "http://example.org/subject"
+        assert subj_prop_path_assertion.assertion_path == assertion_path
 
 
-@pytest.mark.usefixtures("target_store_access")
-def test_subject_str_check(target_store_access):
+@pytest.mark.usefixtures("decorated_rdf_stores")
+def test_subject_str_check(decorated_rdf_stores):
     assertion_path = AssertPath("http://example.org/subject")
-    subj_prop_path_assertion = subj.SubjPropPathAssertion(
-        "http://example.org/subject",
-        assertion_path,
-        target_store_access,
-        {},
-        "urn:test",
-    )
 
-    assert (
-        subj_prop_path_assertion._subject_str_check(
-            "http://example.org/subject"
+    for rdf_store in decorated_rdf_stores:
+        assertion_path = AssertPath("http://example.org/subject")
+        subj_prop_path_assertion = SubjPropPathAssertion(
+            "http://example.org/subject",
+            assertion_path,
+            rdf_store,
+            {},
+            "urn:test",
         )
-        == "http://example.org/subject"
-    )
 
-    # check now when subject is rdflib.query.ResultRow and URIRef
-
-    assert (
-        subj_prop_path_assertion._subject_str_check(
-            URIRef("http://example.org/subject")
+        assert (
+            subj_prop_path_assertion._subject_str_check(
+                "http://example.org/subject"
+            )
+            == "http://example.org/subject"
         )
-        == "http://example.org/subject"
-    )
 
-    assert (
-        subj_prop_path_assertion._subject_str_check(
-            ResultRow({"subject": "http://example.org/subject"}, ["subject"])
+        # check now when subject is rdflib.query.ResultRow and URIRef
+
+        assert (
+            subj_prop_path_assertion._subject_str_check(
+                URIRef("http://example.org/subject")
+            )
+            == "http://example.org/subject"
         )
-        == "http://example.org/subject"
-    )
 
-    # test when subject is not uri
-    assert (
-        subj_prop_path_assertion._subject_str_check("example.org/subject")
-        is None
-    )
+        assert (
+            subj_prop_path_assertion._subject_str_check(
+                ResultRow({"subject": "http://example.org/subject"}, ["subject"])
+            )
+            == "http://example.org/subject"
+        )
+
+        # test when subject is not uri
+        assert (
+            subj_prop_path_assertion._subject_str_check("example.org/subject")
+            is None
+        )
 
 
 if __name__ == "__main__":
